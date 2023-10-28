83e0c0b3709a6b6a355a96f351aea0707e5c14a6
Lovealone1 <89592158+Lovealone1@users.noreply.github.com>
2023-10-28 01:42:39 -0500

    Feat: Creacion de la barra lateral, Feat: Creacion del login, Feat: Conexion del login con el backend

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <!--<Sidebar/>-->
     <router-view/>
   </div>
 </template>
-
+<script>
+import Sidebar from '@/components/Sidebar.vue';
+export default{
+  components: {
+    Sidebar
+  }
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -15,18 +19,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100vh !important;
+}
+a{
+  color: #4a4a4e !important;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
