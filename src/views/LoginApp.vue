5669c11c727e30a611937a53889fc61908d3446d
JhnnyPzG <jpaz152@soyudemedellin.edu.co>
2023-11-16 21:56:08 -0500

    Feat: Validaciones

--- src/views/LoginApp.vue
+++ src/views/LoginApp.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="d-flex align-items-center bg-auth border-top border-top-2 border-primary" style="height: 100%; !important">
-        <div class="container">
+  <div class="d-flex align-items-center bg-auth border-top border-top-2 border-primary" style="height: 100%; !important">
+    <div class="container">
       <div class="row justify-content-center">
         <div class="col-12 col-md-5 col-xl-4 my-5">
 
@@ -17,7 +17,7 @@
           <!-- Form -->
           <form>
             <div v-if="msm_error" class="alert alert-danger" roles="alert">
-                {{ msm_error }}
+              {{ msm_error }}
             </div>
             <!-- Email address -->
             <div class="form-group">
@@ -69,65 +69,102 @@
 
         </div>
       </div> <!-- / .row -->
-        </div> <!-- / .container -->
-    </div>
-
+    </div> <!-- / .container -->
+  </div>
 </template>
   
-  <script>
-  import axios from 'axios';
-  import store from '@/store/index';
+<script>
+import axios from 'axios';
+import store from '@/store/index';
+import Swal from 'sweetalert2';
 
-  export default {
-    name: 'LoginApp',
-    data(){
-        return{
-            email: '',
-            password: '',
-            msm_error: ''
-        }
-    },
-    created(){
-        console.log(this.$url);
-    },
-    methods: {
-        validar(){
-            if(!this.email){
-                this.msm_error = 'Ingrese un correo electrónico'
-            }else if(!this.password){
-                this.msm_error = 'Ingrese una contraseña'
-            }else{
-                this.msm_error = '';
-                this.login();
-            }
-        },
-        login(){
-            let data = {
-                email: this.email,
-                password: this.password
-            }
-            axios.post(this.$url+'/login_usuario',data,{
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            }).then((result)=>{
-                if(result.data.data == undefined){
-                    this.msm_error = result.data.message; 
-                }
-                if(result.data.token){
-                    this.$store.dispatch('saveToken',result.data.token);
-                    this.$router.push({name: 'dashboard'});
-                }
-            }).catch((error)=>{
-                console.log(error);
-            });
-        },
-        logout(){
-          this.$store.dispatch('logout');
-        }
-    },
-    components: {
+export default {
+  name: 'LoginApp',
+  data() {
+    return {
+      email: '',
+      password: '',
+      msm_error: ''
     }
+  },
+  created() {
+    console.log(this.$url);
+  },
+  methods: {
+    validar() {
+      if (!this.email) {
+        Swal.fire({
+          title: 'Ingrese un correo electrónico',
+          icon: 'error'
+        });
+      } else if (!this.password) {
+        Swal.fire({
+          title: 'Ingrese una contraseña',
+          icon: 'error'
+        });
+      } else {
+        this.msm_error = '';
+        this.login();
+      }
+    },
+
+    login() {
+      let data = {
+        email: this.email,
+        password: this.password
+      }
+
+      if (!this.email || !this.password) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Por favor, rellena todos los campos.',
+        });
+        return;
+      }
+
+      axios.post(this.$url + '/login_usuario', data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then((result) => {
+        if (result.data.data == undefined) {
+          this.msm_error = result.data.message;
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: this.msm_error,
+          });
+        }
+        if (result.data.token) {
+          this.$store.dispatch('saveToken', result.data.token);
+          this.$router.push({ name: 'dashboard' });
+          Swal.fire({
+            icon: 'success',
+            title: 'Éxito',
+            text: 'Has iniciado sesión correctamente.',
+          });
+        }
+      }).catch((error) => {
+        console.log(error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Ha ocurrido un error al iniciar sesión.',
+        });
+      });
+    },
+    logout() {
+      this.$store.dispatch('logout');
+      Swal.fire({
+        icon: 'success',
+        title: 'Éxito',
+        text: 'Has cerrado sesión correctamente.',
+      });
+    }
+  },
+  components: {
   }
-  </script>
+}
+</script>
   
